bc8757118dced47d7650f1484aee512fd9714531
Alexey_Vittsenko <bananac1de@yandex.ru>
2025-01-09 18:45:13 +0500

    Задача сделана

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var changeButton: UIButton!
+    @IBOutlet private weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        changeButton.layer.cornerRadius = changeButton.bounds.height / 5
+        
     }
-
-
+    
+    private var count = 0
+    
+    @IBAction private func buttonDidTap(_ sender: Any) {
+        count += 1
+        counterLabel.text = "Значение счётчика: \(count)"
+    }
+    
 }
-
