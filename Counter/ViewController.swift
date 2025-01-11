8314c0182f1fed0b6d5d76676a41b7f12da29ff6
Alexey_Vittsenko <bananac1de@yandex.ru>
2025-01-11 17:38:41 +0500

    Внесены правки

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -12,15 +12,14 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var changeButton: UIButton!
     @IBOutlet private weak var counterLabel: UILabel!
     
+    private var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         changeButton.layer.cornerRadius = changeButton.bounds.height / 5
         
     }
-    
-    private var count = 0
     
     @IBAction private func buttonDidTap(_ sender: Any) {
         count += 1
